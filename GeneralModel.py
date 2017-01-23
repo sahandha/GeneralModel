a81216686b3aa3efeec62f08dd1091ce3b05450a
Sahand Hariri <sahandha@gmail.com>
2017-01-22 19:49:57 -0600

    Kuramoto works

--- GeneralModel.py
+++ GeneralModel.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pylab as plt
+plt.rc('font', family='serif')
 import warnings
 import sys
 
@@ -17,6 +18,7 @@ class GeneralModel:
         self._params = params
 
         self._Models = {
+            "Kuramoto"  : self.KuramotoFlow,
             "SIR"       : self.SIRFlow,
             "VanderPol" : self.VanderPolFlow
             }
@@ -34,16 +36,24 @@ class GeneralModel:
         self._XX  = np.zeros((len(self._Time),len(self._x)))
         self._dXX = np.zeros((len(self._Time),len(self._dx)))
 
+    def KuramotoFlow(self, t, x, params):
+        k   = params["k"]
+        dx = np.zeros(len(x))
+        self._x = np.mod(x, 2*np.pi)
+        x = self._x
+        for ii in xrange(len(x)/2):
+            dx[ii] = params["w_{}".format(ii)] + k * np.sum([np.sin(tj-x[ii]) for tj in x])
+        for ii in xrange(len(x)/2,len(x)):
+            dx[ii] = 0
+        return dx
+
     def PlotState(self, fignum=1, states=[],legend=[],colors=[], releaseplot=True):
 
         if len(states) == 0:
             warnings.warn("No state variables specified. Plotting all.")
             statesymbols = np.arange(self._dim)
         if len(colors) == 0:
-            colors = [[float(self._dim-x)/self._dim,
-                       np.mod(float(self._dim-x)/self._dim - 0.5,1),
-                       np.mod(float(self._dim-x)/self._dim,1)]
-                       for x in np.arange(self._dim)]
+            colors = [[np.random.uniform(0,1) for _ in range(3)] for _ in np.arange(self._dim)]
 
 
         if states == "All":
@@ -85,7 +95,6 @@ class GeneralModel:
             warnings.warn("No state variables specified. Plotting all.")
             statesymbols = np.arange(self._dim)
 
-
         stateind = states.keys();
         plt.figure(fignum)
         plt.suptitle("Phase Plot of the "+self._Name+" Model")
@@ -95,6 +104,75 @@ class GeneralModel:
         plt.ylabel(states.values()[1])
         plt.grid(True)
 
+        if releaseplot:
+            plt.show()
+
+    def PolarPlotState(self, fignum=3, states={"r":np.array([1]), "theta":np.array([2])}, statelabels=[],legend=[],colors=[], releaseplot=True):
+
+        if len(states) == 0:
+            warnings.warn("No state variables specified. Plotting all.")
+            statelabels = np.arange(self._dim)
+        if len(colors) == 0:
+            colors = colors = [[np.random.uniform(0,1) for _ in range(3)] for _ in np.arange(self._dim)]
+
+        if states == "All":
+            numplots = self._d
+        else:
+            numplots = len(legend)
+
+        rinds     = states["r"]
+        thetainds = states["theta"]
+
+        plt.figure(fignum)
+        plt.suptitle("Time Evolution of the "+self._Name+" Model")
+        for i in xrange(numplots):
+            if len(legend)==0:
+
+                plt.subplot(2*numplots,1,i+1)
+                ps = plt.plot(self._Time,
+                              self._XX[:,rinds[i]]*np.cos(self._XX[:,thetainds[i]]))
+
+                plt.subplot(2*numplots,1,i+2)
+                ps = plt.plot(self._Time,
+                              self._XX[:,rinds[i]]*np.sin(self._XX[:,thetainds[i]]))
+            else:
+                plt.subplot(2,1,1)
+                ps1 = plt.plot(self._Time,
+                              self._XX[:,rinds[i]]*np.cos(self._XX[:,thetainds[i]]),label="{}".format(legend[i]))
+                plt.subplot(2,1,2)
+                ps2 = plt.plot(self._Time,
+                              self._XX[:,rinds[i]]*np.sin(self._XX[:,thetainds[i]]),label="{}".format(legend[i]))
+
+
+            plt.setp(ps1, 'Color', colors[i], 'linewidth', 3)
+            plt.setp(ps2, 'Color', colors[i], 'linewidth', 3)
+            plt.grid(True)
+
+
+        if len(legend)!=0:
+            plt.xlabel("Time")
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+
+        if releaseplot:
+            plt.show()
+
+    def PolarPlotPhase(self, fignum=4, states={"r":1, "theta":2}, statelabels=["x","y"],color="k",releaseplot=True):
+        if len(states) == 0:
+            warnings.warn("No state variables specified. Plotting all.")
+            statelabels = np.arange(self._dim)
+
+        stateind = states.keys();
+        r     = self._XX[:,states["r"]    -1]
+        theta = self._XX[:,states["theta"]-1]
+
+        plt.figure(fignum)
+        plt.suptitle("Polar Phase Plot of the "+self._Name+" Model")
+        ps = plt.plot(r*np.cos(theta),r*np.sin(theta))
+        plt.setp(ps, 'Color', color, 'linewidth', 3)
+        plt.xlabel(statelabels[0])
+        plt.ylabel(statelabels[1])
+        plt.grid(True)
 
         if releaseplot:
             plt.show()
@@ -108,7 +186,7 @@ class GeneralModel:
                  "+self._Name+" model. \n \
                  Please define a flow method and pass it to the simulate method.")
             else:
-                self.Flow = flow
+                self.Flow = Flow
 
     def SetUpdateMethod(self, updatemethod):
         try:
@@ -127,21 +205,21 @@ class GeneralModel:
     def Simulate(self, Flow=None, UpdateMethod="RungeKutta"):
         self.SetUpdateMethod(UpdateMethod)
         self.SetFlow(Flow)
-        self._dx = np.array(self.Flow(self._t, self._x))
+        self._dx = np.array(self.Flow(self._t, self._x, self._params))
         for ii in range(len(self._Time)):
             self._XX[ii,:] = self._x;
             self._dXX[ii,:] = self._dx;
             self.Update();
 
-    def SIRFlow(self, t, state):
+    def SIRFlow(self, t, state, params):
         if len(self._params)==0:
             beta  = 4
             gamma = 1
             N     = 1
         else:
-            beta  = self._params["beta"]
-            gamma = self._params["gamma"]
-            N     = self._params["N"]
+            beta  = params["beta"]
+            gamma = params["gamma"]
+            N     = params["N"]
         S, I , R = state[0], state[1], state[2]
         dS = -beta*I*S/N;
         dI =  beta*I*S/N - gamma*I
@@ -149,24 +227,24 @@ class GeneralModel:
         return np.array([dS, dI, dR])
 
     def UpdateEuler(self):
-        self._dx = self.Flow(self._t, self._x);
+        self._dx = self.Flow(self._t, self._x, self._params);
         self._x  = self._x + self._dt*self._dx;
         self._t  = self._t + self._dt
 
     def UpdateRK(self):
-        k1 = self.Flow(self._t, self._x)
-        k2 = self.Flow(self._t+self._dt/2, self._x+k1*self._dt/2)
-        k3 = self.Flow(self._t+self._dt/2, self._x+k2*self._dt/2)
-        k4 = self.Flow(self._t+self._dt  , self._x+k2*self._dt)
+        k1 = self.Flow(self._t, self._x, self._params)
+        k2 = self.Flow(self._t+self._dt/2, self._x+k1*self._dt/2,  self._params)
+        k3 = self.Flow(self._t+self._dt/2, self._x+k2*self._dt/2,  self._params)
+        k4 = self.Flow(self._t+self._dt  , self._x+k2*self._dt  ,  self._params)
 
         self._x = self._x + (k1 + 2*k2 + 2*k3 + k4)*self._dt/6
         self._t = self._t + self._dt
 
-    def VanderPolFlow(self, t, state):
+    def VanderPolFlow(self, t, state, params):
         if len(self._params)==0:
             mu = 0.5
         else:
-            mu = self._params["mu"]
+            mu = params["mu"]
         x, y = state[0], state[1]
 
         dx = y
