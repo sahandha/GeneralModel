417566269d133773287996e6a219605bb50d4117
Sahand Hariri <sahand@meb-0220c-01.mechse.illinois.edu>
2017-01-30 14:44:48 -0600

    turn in into a package

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! python3
+
+from setuptools import setup
+
+setup(
+    name='GeneralModel',
+    version='0.1',
+    description='A quick prototyping module for  ODE model simulation',
+    url='http://github.com/sahandha/GeneralModel',
+    author='Sahand Hariri',
+    author_email='sahandha@gmail.com',
+    license='open',
+    packages=['GeneralModel'],
+    zip_safe=False
+    )
